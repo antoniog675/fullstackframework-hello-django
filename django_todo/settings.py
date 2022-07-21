eb4c93f1f7d8f47810de28b13f369072e78dd794
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-07-21 16:21:21 +0000

    Host issue

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['git.heroku.com/antoniog675-django-todo-app/']
+ALLOWED_HOSTS = ['antoniog675-django-todo-app']
 
 
 # Application definition
