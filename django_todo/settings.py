ff01385d61de66145ff9a45c8d55ab57c336f548
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-07-21 16:44:22 +0000

    Added automatic deplyment, fixing host issue

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get(HEROKU_HOSTNAME)]
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
